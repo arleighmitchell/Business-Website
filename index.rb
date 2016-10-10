3ebb56ac1104638d2bfd0bf6843d04b95e54141b
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-10-10 09:23:50 -0700

    Add styling

--- index.rb
+++ index.rb
@@ -3,34 +3,43 @@ require 'sendgrid-ruby'
 
 get '/' do
   "home"
-  @names = ["joey", "mike", "anthony"]
+  @family = ["Mother: Janie", "Father: Tony", "Brother: Clemens", "Sister: Arleigh"]
   erb :home
 end
 
 get'/photos' do
-  "here are some pictures"
+  "Pictures of Molly"
+  erb :photos
 end
 
 get '/history' do
-  "Hello World"
+  "The History of Miss Molly"
+  erb :history
 end
 
 get '/contact' do
-  p "contact us"
+  "Get in Touch with Molly"
+  erb :contact
+end
 
-  from = SendGrid::Email.new(email: 'arleighmitchell@gmail.com')
-  subject = 'Hello World from the SendGrid Ruby Library!'
-  to = SendGrid::Email.new(email: 'arleighmitchell@gmail.com')
-  content = SendGrid::Content.new(type: 'text/plain', value: 'Hello, Email!')
+post '/contact-form' do
+  erb :contactform
+    from = SendGrid::Email.new(email: 'arleighmitchell@gmail.com')
+    subject = 'Hello World from the SendGrid Ruby Library!'
+    to = SendGrid::Email.new(email: 'arleighmitchell@gmail.com')
+    content = SendGrid::Content.new(type: 'text/plain', value: 'Hello, Email!')
 
-  mail = SendGrid::Mail.new(from, subject, to, content)
+    mail = SendGrid::Mail.new(from, subject, to, content)
 
-  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 
-  response = sg.client.mail._('send').post(request_body: mail.to_json)
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
 
-  p "omg"
+    p response.status_code
+    p response.body
+end
 
-  p response.status_code
-  p response.body
+get'/contact-form' do
+  "hello"
+  erb :contactform
 end
